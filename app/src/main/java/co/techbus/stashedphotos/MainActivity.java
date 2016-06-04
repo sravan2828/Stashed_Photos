4bea9b3b9cbd065011851655017c6be0868f1f16
sravan kumar <techbus.in@gmail.com>
2016-06-04 22:13:10 +0530

    commit after api integration

--- app/src/main/java/co/techbus/stashedphotos/MainActivity.java
+++ app/src/main/java/co/techbus/stashedphotos/MainActivity.java
@@ -1,26 +1,9 @@
 package co.techbus.stashedphotos;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import co.techbus.stashedphotos.adapter.FeedListAdapter;
-import co.techbus.stashedphotos.app.AppController;
-import co.techbus.stashedphotos.data.FeedItem;
-
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Log;
 import android.widget.ListView;
 
 import com.android.volley.Cache;
@@ -31,12 +14,24 @@ import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import co.techbus.stashedphotos.adapter.FeedListAdapter;
+import co.techbus.stashedphotos.app.AppController;
+import co.techbus.stashedphotos.data.FeedItem;
+
 public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private ListView listView;
     private FeedListAdapter listAdapter;
     private List<FeedItem> feedItems;
-    private String URL_FEED = "http://api.androidhive.info/feed/feed.json";
+    private String URL_FEED = "http://services.stashcity.com/Service.svc/GetAllRecentImages";
 
     @SuppressLint("NewApi")
     @Override
@@ -68,12 +63,13 @@ public class MainActivity extends Activity {
 
         } else {
             // making fresh volley request and getting json
-            JsonObjectRequest jsonReq = new JsonObjectRequest(Method.GET,
+            JsonObjectRequest jsonReq = new JsonObjectRequest(Method.POST,
                     URL_FEED, null, new Response.Listener<JSONObject>() {
 
                 @Override
                 public void onResponse(JSONObject response) {
                     VolleyLog.d(TAG, "Response: " + response.toString());
+                    Log.e("responce", response.toString());
                     if (response != null) {
                         parseJsonFeed(response);
                     }
@@ -97,22 +93,22 @@ public class MainActivity extends Activity {
      * */
     private void parseJsonFeed(JSONObject response) {
         try {
-            JSONArray feedArray = response.getJSONArray("feed");
+            JSONArray feedArray = response.getJSONArray("GetAllRecentImagesResult");
 
             for (int i = 0; i < feedArray.length(); i++) {
                 JSONObject feedObj = (JSONObject) feedArray.get(i);
 
                 FeedItem item = new FeedItem();
-                item.setId(feedObj.getInt("id"));
-                item.setName(feedObj.getString("name"));
+                item.setId(feedObj.getInt("photograpId"));//id
+                item.setName(feedObj.getString("name"));//status
 
                 // Image might be null sometimes
-                String image = feedObj.isNull("image") ? null : feedObj
-                        .getString("image");
+                String image = feedObj.isNull("url") ? null : feedObj
+                        .getString("url");//main image of post
                 item.setImge(image);
-                item.setStatus(feedObj.getString("status"));
-                item.setProfilePic(feedObj.getString("profilePic"));
-                item.setTimeStamp(feedObj.getString("timeStamp"));
+                item.setStatus(feedObj.getString("name"));//status
+                item.setProfilePic(feedObj.getString("url"));//profilepic
+                item.setTimeStamp("1403375851930");//feedObj.getString("timeStamp"));//timestamp
 
                 // url might be null sometimes
                 String feedUrl = feedObj.isNull("url") ? null : feedObj
